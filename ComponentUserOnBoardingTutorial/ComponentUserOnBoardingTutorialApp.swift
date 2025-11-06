5c7a6c2eb4f651ef1666fda1fd832c2be3fe4ef8
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-11-06 10:29:42 +0700

    Initial Commit

---
+++ ComponentUserOnBoardingTutorial/ComponentUserOnBoardingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentUserOnBoardingTutorialApp.swift
+//  ComponentUserOnBoardingTutorial
+//
+//  Created by Iqbal Alhadad on 06/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ComponentUserOnBoardingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
